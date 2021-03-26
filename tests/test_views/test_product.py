092757220e8d828c235ba2b3fd9e3dab86a89a63
Pavel <giropavel85@gmail.com>
2021-03-26 13:42:30 +0300

    create tests

---
+++ tests/test_views/test_product.py
@@ -0,0 +1,24 @@
+from pytest import fixture
+
+from app import app
+
+from views.product import get_default_products
+
+
+@fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_get_default_products():
+    res = get_default_products()
+    assert isinstance(res, dict)
+    assert all(map(lambda x: isinstance(x, int), res.keys()))  # проверим что все ключи у нас являются int
+
+
+# тест на апи
+def test_recover_products(client):
+    resp = client.post("/products/recover/")
+    data = resp.json
+    assert data == {"ok": True}
