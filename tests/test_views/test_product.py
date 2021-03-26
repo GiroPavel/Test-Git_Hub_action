2228d7ae6d4a78d69ee1cf646f77b3cccd6aced3
Pavel <giropavel85@gmail.com>
2021-03-26 16:06:57 +0300

    run tests in workflow action

--- tests/test_views/test_product.py
+++ tests/test_views/test_product.py
@@ -8,7 +8,10 @@ from views.product import get_default_products
 @fixture
 def client():
     with app.test_client() as client:
+        # мы делаем йилд и он у нас берется из этой функции, те вытаскивается генератором,
+        # отдается сним что-то делается, а потом фикстура закрывается и выходит из этого кентеста with.
         yield client
+        # return client  # на ретерн автоматически закрывается контекст
 
 
 def test_get_default_products():
